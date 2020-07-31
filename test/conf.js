779c794c507ebb1a40636520683d692e015d90b8
HeatherButterfield <heather@rnetwork.io>
2020-07-31 13:59:29 -0600

    test20

--- test/conf.js
+++ test/conf.js
@@ -1,4 +1,7 @@
 exports.config = {
+  seleniumAddress: 'https://rNetwork:479bb1d7-c3b0-4748-b13d-223aee158bb9@ondemand.us-west-1.saucelabs.com:443/wd/hub',
+  sauceUser: 'rNetwork',
+  sauceKey: '479bb1d7-c3b0-4748-b13d-223aee158bb9',
   framework: 'jasmine',
   specs: ['spec.js'],
   SELENIUM_PROMISE_MANAGER: false,
