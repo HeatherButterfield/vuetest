9c7918702544911a80d77f2d126d0e402c309f66
HeatherButterfield <heather@rnetwork.io>
2020-07-31 15:42:58 -0600

    test21

--- test/conf.js
+++ test/conf.js
@@ -6,6 +6,7 @@ exports.config = {
   specs: ['spec.js'],
   SELENIUM_PROMISE_MANAGER: false,
   jasmineNodeOpts: {
-    defaultTimeoutInterval: 30000
+    defaultTimeoutInterval: 30000,
+    showColors: true,
   },
 };
