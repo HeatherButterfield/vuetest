00fb2078101e7f2a5592d146e40883618c48ceb7
HeatherButterfield <heather@rnetwork.io>
2020-07-31 11:26:32 -0600

    test6

--- test/conf.js
+++ test/conf.js
@@ -1,5 +1,6 @@
 exports.config = {
   directConnect: true,
+  chromeDriver: '/app/.chromedriver/bin/chromedriver',
   framework: 'jasmine',
   specs: ['spec.js'],
   SELENIUM_PROMISE_MANAGER: false,
